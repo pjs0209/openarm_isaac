6106adeb411cf9d1c5335f17adcff1f7375d5332
jun <pjs0209@konkuk.ac.kr>
2026-02-25 20:45:55 +0900

    feat : real_to_sim launch

--- src/openarm_converters/setup.py
+++ src/openarm_converters/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'bimanual_converter = openarm_converters.bimanual_joint_state_converter:main'
+            'bimanual_converter = openarm_converters.bimanual_joint_state_converter:main',
+            'physical_leader_relay = openarm_converters.physical_leader_relay:main',
         ],
     },
 )
